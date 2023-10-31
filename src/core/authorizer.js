2ae5be423b120b28ff54c8cb34e37900bfc4cd59
sswahn <36422765+sswahn@users.noreply.github.com>
2023-10-31 16:38:42 -0400

    Create authorizer.js

---
+++ src/core/authorizer.js
@@ -0,0 +1,26 @@
+import { CognitoIdentityProviderClient, GetUserCommand } from '@aws-sdk/client-cognito-identity-provider'
+
+export const authorizer = async (event, callback) => {
+  try {
+    if (!event.headers.Cookie || !event.headers.Cookie.includes('id=')) {
+      callback('Unauthorized')
+    }
+    const token = event.headers.Cookie.split('id=')[1].split(';')[0]
+    const client = new CognitoIdentityProviderClient()
+    const command = new GetUserCommand({ AccessToken: token })
+    await client.send(command)
+    callback(null, {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [      {
+          Action: 'execute-api:Invoke',
+          Effect: 'Allow',
+          Resource: event.methodArn
+        }]
+      }
+    })
+  } catch (error) {
+    callback('Unauthorized')
+  }
+}
